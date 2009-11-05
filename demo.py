b1bda8156edcfc34051cf109a86c374d5322b3bb
smee <om@webdrive.ru>
2009-11-05 12:56:27 +0300

    dump_desk() & fold_trace()

--- demo.py
+++ demo.py
@@ -1,48 +1,41 @@
+#!/usr/bin/python
 import time
 import os, sys
 import corners
 
-if sys.platform == 'win32':            
-    def clear_screen(): 
+if sys.platform == 'win32':
+    def clear_screen():
         os.system('cls')
 else:
-    def clear_screen(): 
-        os.system('clear')        
+    def clear_screen():
+        os.system('clear')
 
 #def clear_screen():
 #    print '---'
-    
-def main():    
+
+def main():
     g = corners.Game()
     pause = 1
-    small_pause = .1
-    
+    small_pause = 0.1
+
     clear_screen()
-    g.show_desk()
-    
-    while not g.is_gameover():        
-        g.set_move_white()
-        
-        trace = g.best_step()         
-        for pfrom, pto in zip(trace[:-1], trace[1:]):            
+    corners.dump_desk(g)
+
+    def make_move():
+        trace = g.best_step()
+        for pfrom, pto in zip(trace[:-1], trace[1:]):
             g.move(pfrom, pto)
             clear_screen()
-            g.show_desk()
+            corners.dump_desk(g)
             time.sleep(small_pause)
         time.sleep(pause)
-        
+
+    while not g.is_gameover():
+        g.set_move_white()
+        make_move()
+
         g.set_move_black()
-        
-        trace = g.best_step()         
-        for pfrom, pto in zip(trace[:-1], trace[1:]):            
-            g.move(pfrom, pto)
-            clear_screen()
-            g.show_desk()
-            time.sleep(small_pause)
-                        
-        time.sleep(pause)        
-        print 
-        
+        make_move()
+
 if __name__ == '__main__':
     main()
-    
